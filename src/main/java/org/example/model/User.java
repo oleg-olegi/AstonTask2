8413f7af7743bebc81c6a757fe7bf72ee7b6d599
oleg-olegi <ol.shink87@gmail.com>
2024-07-11 08:53:43 +0300

    create entity classes

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private List<Post> posts; // OneToMany relationship
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
