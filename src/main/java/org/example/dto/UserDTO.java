8a6084c7271cd3f47e8dc021b339bce82cba5d99
oleg-olegi <ol.shink87@gmail.com>
2024-07-11 09:04:55 +0300

    create dto classes

---
+++ src/main/java/org/example/dto/UserDTO.java
@@ -0,0 +1,44 @@
+package org.example.dto;
+
+import java.util.List;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private List<PostDTO> posts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<PostDTO> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<PostDTO> posts) {
+        this.posts = posts;
+    }
+
+    // Getters and Setters
+}
