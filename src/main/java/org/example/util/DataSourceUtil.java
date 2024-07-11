afc8a34979ca00668260aa198e0a3abe688ac855
oleg-olegi <ol.shink87@gmail.com>
2024-07-11 08:43:13 +0300

    make structure of project, do changes into pom.xml, create db.properties, DataSourceUtil class

---
+++ src/main/java/org/example/util/DataSourceUtil.java
@@ -0,0 +1,34 @@
+package org.example.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import javax.sql.DataSource;
+
+public class DataSourceUtil {
+    private static HikariDataSource dataSource;
+
+    static {
+        try (InputStream input = DataSourceUtil.class.getClassLoader().getResourceAsStream("db.properties")) {
+            Properties properties = new Properties();
+            properties.load(input);
+
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(properties.getProperty("jdbc.url"));
+            config.setUsername(properties.getProperty("jdbc.username"));
+            config.setPassword(properties.getProperty("jdbc.password"));
+            config.setDriverClassName(properties.getProperty("jdbc.driverClassName"));
+
+            dataSource = new HikariDataSource(config);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+}
