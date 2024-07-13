3351e905c7cbafb6cdba2618d515bcb6df165e2f
oleg-olegi <ol.shink87@gmail.com>
2024-07-13 13:06:21 +0300

    UserDAOTest and PostDAOTest

--- src/test/java/org/example/dao/UserDAOTest.java
+++ src/test/java/org/example/dao/UserDAOTest.java
@@ -3,11 +3,8 @@ package org.example.dao;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.example.dto.UserDTO;
 import org.example.model.User;
-import org.example.service.UserService;
 
-import org.example.util.DataSourceUtil;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,19 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 public class UserDAOTest {
@@ -44,23 +35,13 @@ public class UserDAOTest {
 
     @BeforeAll
     public static void setUp() {
-
-//        DataSourceUtil datasourceUtil = new DataSourceUtil();
-//        System.setProperty("jdbc.url", postgresContainer.getJdbcUrl());
-//        System.setProperty("jdbc.username", postgresContainer.getUsername());
-//        System.setProperty("jdbc.password", postgresContainer.getPassword());
-//        System.setProperty("jdbc.driverClassName", "org.postgresql.Driver");
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(postgresContainer.getJdbcUrl());
         config.setUsername(postgresContainer.getUsername());
         config.setPassword(postgresContainer.getPassword());
         config.setDriverClassName("org.postgresql.Driver");
-
         dataSource = new HikariDataSource(config);
-
-//        dataSource = DataSourceUtil.getDataSource();
         userDAO = new UserDAO(dataSource);
-
         try (var connection = dataSource.getConnection(); var stmt = connection.createStatement()) {
             stmt.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))");
         } catch (SQLException e) {
@@ -68,8 +49,24 @@ public class UserDAOTest {
         }
     }
 
+    @BeforeEach
+    public void cleanData() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(postgresContainer.getJdbcUrl());
+        config.setUsername(postgresContainer.getUsername());
+        config.setPassword(postgresContainer.getPassword());
+        config.setDriverClassName("org.postgresql.Driver");
+        dataSource = new HikariDataSource(config);
+        userDAO = new UserDAO(dataSource);
+        try (var connection = dataSource.getConnection(); var stmt = connection.createStatement()) {
+            stmt.execute("DELETE FROM users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @BeforeAll
-    public static void tearup() {
+    public static void start() {
         postgresContainer.start();
     }
 
@@ -80,7 +77,7 @@ public class UserDAOTest {
     }
 
     @Test
-    public void testSaveAndGetById() throws SQLException {
+    public void testSave() throws SQLException {
         User user = new User();
         user.setName("John Doe");
         user.setEmail("john.doe@example.com");
@@ -92,40 +89,82 @@ public class UserDAOTest {
         assertEquals("john.doe@example.com", retrievedUser.getEmail());
     }
 
-//    @Test
-//    public void testUpdateUser() throws SQLException {
-//        UserDTO userDTO = new UserDTO(null, "Jane Smith", "jane.smith@example.com");
-//
-//        userService.saveUser(userDTO);
-//
-//        List<UserDTO> users = userService.getAllUsers();
-//        UserDTO savedUser = users.get(0);
-//
-//        savedUser.setName("Updated Name");
-//        savedUser.setEmail("updated.email@example.com");
-//
-//        userService.updateUser(savedUser);
-//
-//        UserDTO updatedUser = userService.getUserById(savedUser.getId());
-//        assertEquals(savedUser.getName(), updatedUser.getName());
-//        assertEquals(savedUser.getEmail(), updatedUser.getEmail());
-//    }
-//
-//    @Test
-//    public void testDeleteUser() throws SQLException {
-//        UserDTO userDTO = new UserDTO(null, "Alice Brown", "alice.brown@example.com");
-//
-//        userService.saveUser(userDTO);
-//
-//        List<UserDTO> usersBeforeDeletion = userService.getAllUsers();
-//        assertEquals(1, usersBeforeDeletion.size());
-//
-//        UserDTO savedUser = usersBeforeDeletion.get(0);
-//        Long userId = savedUser.getId();
-//
-//        userService.deleteUser(userId);
-//
-//        List<UserDTO> usersAfterDeletion = userService.getAllUsers();
-//        assertTrue(usersAfterDeletion.isEmpty());
-//    }
+    @Test
+    public void testGetById() throws SQLException {
+        User user = new User();
+        user.setId(1L);
+        user.setName("John Doe");
+        user.setEmail("john.doe@example.com");
+        userDAO.save(user);
+
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setName("Sarah Connor");
+        user2.setEmail("sarah@example.com");
+        userDAO.save(user2);
+
+        User retrievedUser = userDAO.getById(user.getId());
+        assertNotNull(retrievedUser);
+        assertEquals("John Doe", retrievedUser.getName());
+        assertEquals("john.doe@example.com", retrievedUser.getEmail());
+
+        User retrievedUser2 = userDAO.getById(user2.getId());
+        assertNotNull(retrievedUser2);
+        assertEquals("Sarah Connor", retrievedUser2.getName());
+        assertEquals("sarah@example.com", retrievedUser2.getEmail());
+    }
+
+    @Test
+    public void GetByIdReturnNull() throws SQLException {
+        User user = new User();
+        user.setId(1L);
+        user.setName("John Doe");
+        user.setEmail("john.doe@example.com");
+        userDAO.save(user);
+
+        User retrievedUser = userDAO.getById(user.getId());
+        assertNotNull(retrievedUser);
+        assertEquals("John Doe", retrievedUser.getName());
+        assertEquals("john.doe@example.com", retrievedUser.getEmail());
+
+        User retrievedNullUser = userDAO.getById(10L);
+        assertNull(retrievedNullUser);
+    }
+
+    @Test
+    public void testUpdateUser() throws SQLException {
+        User userDTO = new User(1L, "Jane Smith", "jane.smith@example.com");
+
+        userDAO.save(userDTO);
+
+        List<User> users = userDAO.getAllUsers();
+        User savedUser = users.get(0);
+
+        savedUser.setName("Updated Name");
+        savedUser.setEmail("updated.email@example.com");
+
+        userDAO.update(savedUser);
+
+        User updatedUser = userDAO.getById(savedUser.getId());
+        assertEquals(savedUser.getName(), updatedUser.getName());
+        assertEquals(savedUser.getEmail(), updatedUser.getEmail());
+    }
+
+    @Test
+    public void testDeleteUser() throws SQLException {
+        User userDTO = new User(1L, "Alice Brown", "alice.brown@example.com");
+
+        userDAO.save(userDTO);
+
+        List<User> usersBeforeDeletion = userDAO.getAllUsers();
+        assertEquals(1, usersBeforeDeletion.size());
+
+        User savedUser = usersBeforeDeletion.get(0);
+        Long userId = savedUser.getId();
+
+        userDAO.delete(userId);
+
+        List<User> usersAfterDeletion = userDAO.getAllUsers();
+        assertTrue(usersAfterDeletion.isEmpty());
+    }
 }
